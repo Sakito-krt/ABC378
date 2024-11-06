53f711609714c68b0cd4e0bc571487eedffc229e
akira <urara000setaetel@gmail.com>
2024-11-06 12:11:02 +0900

    Add A

---
+++ ABC378/A/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] A = Console.ReadLine().Split();
+        Array.Sort(A);
+        int ans = 0 ;
+
+        for (int i = 0 ; i < A.Count()-1 ; i++)
+        {
+            if (A[i] == A[i+1])
+            {
+                ans ++;
+                i ++;
+            }
+        }
+
+        Console.WriteLine(ans);
+    }
+}
